42da3f442741e5faf21d78eb628eada2bac6987e
unknown <rafael.florentino.barbosa@gmail.com>
2024-09-10 17:54:54 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Exo.WebApi.Contexts;
+using Exo.WebApi.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona serviços ao contêiner
+builder.Services.AddScoped<ExoContext, ExoContext>();
+builder.Services.AddControllers();
+builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
+
+var app = builder.Build();
+
+// Configura o middleware
+app.UseRouting();
+
+// Registra os endpoints diretamente
+app.MapControllers();
+
+app.Run();
